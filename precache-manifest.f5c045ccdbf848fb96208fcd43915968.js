c5e2457f24d11b5c6f07746f5617d054567b1688
Musa Musa <musabrillz@gmail.com>
2020-11-22 14:02:20 +0100

    Updates

---
+++ precache-manifest.f5c045ccdbf848fb96208fcd43915968.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df9428a03f3784901a7526d1d5aebb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a254be0548cd74069b",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e91317ec7235a96aa7a2",
+    "url": "/static/css/main.57e6a19c.chunk.css"
+  },
+  {
+    "revision": "c6a254be0548cd74069b",
+    "url": "/static/js/2.60450276.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60450276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91317ec7235a96aa7a2",
+    "url": "/static/js/main.175b090a.chunk.js"
+  },
+  {
+    "revision": "9c2a4cd68397a82fb21f",
+    "url": "/static/js/runtime-main.8319ca0e.js"
+  },
+  {
+    "revision": "7af1339b886b6f1fc97022d28ccd47fb",
+    "url": "/static/media/bell.7af1339b.svg"
+  },
+  {
+    "revision": "a5b213499098eaa4de2ee36dd454d0a3",
+    "url": "/static/media/bi_chevron-down-1.a5b21349.svg"
+  },
+  {
+    "revision": "937d4ce38e16eba8a532c98fab44df5a",
+    "url": "/static/media/bi_chevron-down.937d4ce3.svg"
+  },
+  {
+    "revision": "fd19dd761071e14167ce576cdcbabceb",
+    "url": "/static/media/dart.fd19dd76.svg"
+  },
+  {
+    "revision": "513cbdaab1a4aa247c3c9f9322a4fe48",
+    "url": "/static/media/location.513cbdaa.svg"
+  },
+  {
+    "revision": "5cf5598529b341e05d8c1da1a6ffea67",
+    "url": "/static/media/settings.5cf55985.svg"
+  }
+]);
